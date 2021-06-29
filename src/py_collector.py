34ff414e664ca0d1b8f98a0687fcf04f3cdca4dd
Michael <michael-fore@sbcglobal.net>
2021-06-28 21:38:05 -0600

    signal interface

--- src/py_collector.py
+++ src/py_collector.py
@@ -1,17 +1,19 @@
 from datetime import datetime, timezone, timedelta
 from threading import Thread, Event
-
+ 
 class Scheduler(Thread):
     ''' Controls scheduling for py_curator
     
-    Based heavily on Timer implementation here
+    Based havily on Timer implementation here
     https://github.com/python/cpython/blob/3.9/Lib/threading.py
     '''
     initial_run = True
+    alive = True
     
     def __init__(self, start_time=None, milliseconds=0, seconds=0, days=0,minutes=0, weeks=0,
                  timezone = None, count=1, separator=1 ):
         
+        signal.signal(signal.SIGALRM, self.cancel)
         Thread.__init__(self)
         self.finished = Event()
         self.count = count
@@ -26,7 +28,8 @@ class Scheduler(Thread):
                                milliseconds=milliseconds).total_seconds()
     
     def cancel(self):
-        self.finished.set()
+        #self.finished.set()
+        self.alive = False
         
     def init_run(self, func):
         ''' initial run to handle the wait'''
@@ -39,14 +42,15 @@ class Scheduler(Thread):
         ''' takes in a last run datetime, if the specified time interval + last run = 
         _now, return true, otherwise false'''
         
-        self.init_run(func)
-            
-        self.finished.wait(self.delta)
-        for i in range(self.count):
-            func()
-            self.finished.wait(self.separator_delta)
-            
-        self.schedule(func)
+        while alive:
+            self.init_run(func)
+
+            self.finished.wait(self.delta)
+            for i in range(self.count):
+                func()
+                self.finished.wait(self.separator_delta)
+
+            self.schedule(func)
 
 class Collector:
     
@@ -57,11 +61,10 @@ class Collector:
     def is_new(self)->bool:
         raise NotImplementedError('Please see documentation for implementation example')
         
-    def orchestrate(self)->None:
+    def orchestrate(self):
         if self.is_new():
             self.upload()
             self.last_run = datetime.now()
         
     def monitor(self)->None:
-        
         self.scheduler.schedule(self.orchestrate)
